8f806186a5f6b4f59b98be04b662d43948de76d0
Rahul Misal <codesrahul96@gmail.com>
2025-05-15 15:00:50 +0530

    OAuth Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
+import OAuthCallback from './components/OAuthCallback';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/" element={<LoginForm />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/callback/:provider" element={<OAuthCallback />} />
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
+    </div>
   );
 }
 
